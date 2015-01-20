9d06ac2e8ff1845bead08b3b55c897877844bb3b
Jagadeesh Madagundi <jmadagun@indiana.edu>
2015-01-20 16:34:46 -0500

    Assignment 1: Final Version

--- shell/xsh_hello.c
+++ shell/xsh_hello.c
@@ -22,9 +22,9 @@ shellcmd xsh_hello(int nargs, char *args[])
     /* Output help, if '--help' argument was supplied */
     if (nargs == 2 && strcmp(args[1], "--help") == 0)
     {
-        printf("Usage: %s\n\n", args[0]);
+        printf("Usage: %s <string>\n\n", args[0]);
         printf("Description:\n");
-        printf("\tDisplays given message.\n");
+        printf("\tDisplays greetings with given string.\n");
         printf("Options:\n");
         printf("\t--help\tdisplay this help and exit\n");
         return OK;
@@ -46,7 +46,7 @@ shellcmd xsh_hello(int nargs, char *args[])
         return SYSERR;
     }
 
-    printf("\n%s\n",args[1]);
+    printf("Hello %s, Welcome to the world of Xinu!!\n",args[1]);
 
     return OK;
 }
