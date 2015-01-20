7f6ac760998bf85dedcd6d55ecfc5912eb778ef2
Jagadeesh Madagundi <jmadagun@indiana.edu>
2015-01-20 14:01:15 -0500

    Assignment 1: Initial version.

---
+++ shell/xsh_hello.c
@@ -0,0 +1,52 @@
+/*
+ * @file     xsh_hello.c
+ *
+ */
+
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * @ingroup shell
+ *
+ * Shell command (hello).
+ * @param nargs  number of arguments in args array
+ * @param args   array of arguments
+ * @return OK for success, SYSERR for syntax error
+ */
+shellcmd xsh_hello(int nargs, char *args[])
+{
+
+
+    /* Output help, if '--help' argument was supplied */
+    if (nargs == 2 && strcmp(args[1], "--help") == 0)
+    {
+        printf("Usage: %s\n\n", args[0]);
+        printf("Description:\n");
+        printf("\tDisplays given message.\n");
+        printf("Options:\n");
+        printf("\t--help\tdisplay this help and exit\n");
+        return OK;
+    }
+
+    /* Check for correct number of arguments */
+    if (nargs > 2)
+    {
+        fprintf(stderr, "%s: too many arguments\n", args[0]);
+        fprintf(stderr, "Try '%s --help' for more information\n",
+                args[0]);
+        return SYSERR;
+    }
+    if (nargs < 2)
+    {
+        fprintf(stderr, "%s: too few arguments\n", args[0]);
+        fprintf(stderr, "Try '%s --help' for more information\n",
+                args[0]);
+        return SYSERR;
+    }
+
+    printf("\n%s\n",args[1]);
+
+    return OK;
+}
