a8c283e7f9600a3a843e91cbee23c38d8de6acbe
Jagadeesh Madagundi <jmadagun@indiana.edu>
2015-03-16 00:26:52 -0400

    update future free code

--- apps/future_cons.c
+++ apps/future_cons.c
@@ -9,9 +9,26 @@ uint future_cons(future *fut) {
     return -1;
   }
   kprintf("consumed %d\n", i);
-  if(fut->flag == FUTURE_EXCLUSIVE)
-    if(future_free(fut)==SYSERR)
+  if(fut->flag == FUTURE_EXCLUSIVE){
+    if(future_free(fut)==SYSERR){
       printf("future_free failed\n");
+    } 
+  }
+  if(fut->flag == FUTURE_SHARED) {
+    if (isempty(fut->get_queue)) { 
+      if(future_free(fut)==SYSERR) {
+        printf("future_free failed\n");
+      }
+      kprintf("future_free success\n");
+    }
+  }
+  if(fut->flag == FUTURE_QUEUE){
+    if (isempty(fut->get_queue) && isempty(fut->set_queue)) {
+      if(future_free(fut)==SYSERR) {
+        printf("future_free failed\n");
+      } 
+    }
+  } 
   return OK;
 }
 
--- include/version.h
+++ include/version.h
@@ -1,1 +1,1 @@
-#define VERSION "(Embedded Xinu) (arm-rpi) #0 (jmadagun@silo.soic.indiana.edu) Sun Mar 15 23:02:50 EDT 2015"
+#define VERSION "(Embedded Xinu) (arm-rpi) #3 (jmadagun@silo.soic.indiana.edu) Mon Mar 16 00:21:23 EDT 2015"
